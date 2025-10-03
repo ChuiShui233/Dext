d5b50d2f14eb99a252c95c01294d2ad13ad8a912
ChuiShui12 <you@example.com>
2025-10-03 22:31:55 +0800

    编译修复

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dext/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const YuMeng233App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
